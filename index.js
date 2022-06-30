f9b94324c173774c6cfc0d85921db0acbe463ffa
Chuck Ringoringo <2370767+u4yk@users.noreply.github.com>
2022-06-30 09:36:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {ue, ue5, capitalize} from './unreal'
 /**
  * A plugin that abstracts communication between a Web UI browser (https://tracerinteractive.com/plugins/webui) and 
  * Unreal Engine.  This will take all items inside a Pinia state and create Pinia actions that will send data to Unreal
- * and window.ue.interface listeners that will update said Pinia state -- which will then trigger Vue updates.
+ * and window.ue.interface listeners that will listen to updates from Unreal -- which will then update the Pinia state.
  */
 export default ({ store, options }) => {
     if (!options.useUnreal) {
